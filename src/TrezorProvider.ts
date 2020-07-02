0c01d9349e6d7407afcc099af4563d4855bfb1b8
avinashpandit <avinash.pandit@gmail.com>
2020-07-01 18:22:28 -0700

    Additional checks on Trezor connection

--- src/TrezorProvider.ts
+++ src/TrezorProvider.ts
@@ -2,9 +2,13 @@ import * as Trezor from "./trezor";
 import { TrezorAccount } from './trezor-account';
 import { NEMLibrary, NetworkTypes, Transaction, SignedTransaction, TransferTransaction,
     TimeWindow, XEM, Address, TransactionHttp, PlainMessage , NemAnnounceResult} from 'nem-library';
+import { TRANSPORT_EVENT,  TRANSPORT} from "trezor-connect";
+import TrezorConnect from "trezor-connect";
 
 class TrezorProvider {
     public initialized: boolean;
+    public connected: boolean;
+    
     public transactionHttp : TransactionHttp;
 
     // 0. This function will bootstrap both the internal nem-library for nem-trezor and the local one
@@ -12,6 +16,7 @@ class TrezorProvider {
     constructor() {
         NEMLibrary.bootstrap(NetworkTypes.MAIN_NET);
         this.initialized = false;
+        this.connected = false;
         this.transactionHttp = new TransactionHttp();
     }
 
@@ -20,9 +25,24 @@ class TrezorProvider {
         {
             const lazyLoad = false;
             const debug = false;
+            if(!this.initialized){    
+                await Trezor.initializeTrezor(debug , lazyLoad);     
+                this.initialized = true;
+                
+                // Listen to TRANSPORT_EVENT
+                TrezorConnect.on(TRANSPORT_EVENT, event => {
+                    if (event.type === TRANSPORT.ERROR) {
+                        // trezor-bridge not installed
+                        this.connected = false;
+                        console.error('Unable to aquire transport. Please check USB connection or see if Trezor Bridge running.')
+                    }
+                    if (event.type === TRANSPORT.START) {
+                        this.connected = true;
+                        console.error('Trezor Connected !!!');
+                    }
+                });
 
-            await Trezor.initializeTrezor(debug , lazyLoad);     
-            this.initialized = true;
+            }
         }
         catch(error){
             console.error('Error in Trezor.initialize ' , error);    
